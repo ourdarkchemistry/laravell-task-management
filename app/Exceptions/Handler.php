355b8de247e3bf018e60218bb01451f4c2af2ac2
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 17:37:20 +0400

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,8 +9,6 @@ class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
-     * @var array
      */
     protected $dontReport = [
         //
@@ -18,8 +16,6 @@ class Handler extends ExceptionHandler
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
      */
     protected $dontFlash = [
         'password',
@@ -28,9 +24,6 @@ class Handler extends ExceptionHandler
 
     /**
      * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
      */
     public function report(Exception $exception)
     {
@@ -39,10 +32,6 @@ class Handler extends ExceptionHandler
 
     /**
      * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
